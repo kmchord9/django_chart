beeee17e6bf2658f080eadde0d2072deed48791d
nmiri-KM <nmiri-km@nmiri-KMnoMacBook-puro.local>
2020-02-21 16:41:48 +0900

    first commit

---
+++ chart/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import TempData
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def get_data(request):
+    labels = ["赤", "青", "黄色", "緑"]
+
+    #, "紫", "橙"
+    data = {
+        "labels": labels,
+        "temp":[d.temp for d in TempData.objects.all()],
+        }
+    return JsonResponse(data)
