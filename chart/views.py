0f3b555798c5bb23f4609b4f36944f4aa3720f86
nmiri-KM <nmiri-km@nmiri-KMnoMacBook-puro.local>
2020-02-25 15:40:40 +0900

    Fix time zone for time data

--- chart/views.py
+++ chart/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import TempData
 import datetime
+#import pytz
+from django.utils import timezone
+
 # Create your views here.
 
 
@@ -9,20 +12,26 @@ def index(request):
     return render(request, 'index2.html')
 
 def get_data(request):
+    #GETリクエストの場合には送られた年月日のデータ
     if "date" in request.GET:
             data = request.GET.get('date').split('-')
             y = data[0]
             m = data[1]
             d = data[2]
-
+            title = '{}/{}/{}'.format(y,m,d)
+    #GETリクエスト出ない場合には今日の日付
     else:
             dt_now = datetime.datetime.now()
             y = dt_now.year
             m = dt_now.month
             d = dt_now.day
+            title = dt_now.strftime("%Y/%m/%d")
 
+    #上記の日付データに基づいてクエリを作成してデータ取得
+    #クエリで得られるタイムゾーンはずれるのでtimezone関数により修正
     data = {
-        "labels": [d.date for d in TempData.objects.filter(date__year=y, date__month=m, date__day=d)],
+        "title": title,
+        "labels": [timezone.localtime(d.date).strftime("%H:%M:%S") for d in TempData.objects.filter(date__year=y, date__month=m, date__day=d)],
         "temp":[d.temp for d in TempData.objects.filter(date__year=y, date__month=m, date__day=d)],
         }
     return JsonResponse(data)
